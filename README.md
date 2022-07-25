563320861624486c1c99bccceaa7cb721de37bc8
lizzieshipwreck <lizzieshipwreck@gmail.com>
2022-07-25 16:08:44 -0700

    doc: add readme and code comments

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # JWST formatter
 
 Quick script to reformat some data for the JWST to use here: https://github.com/nbhasker/JWSTStellarium
+
+Needs proper error handling and some other things.
+
+Here's how to run it (Windows)
+
+1. Open the Node.js command prompt (shortcut in the Windows menu)
+
+![Command Prompt](nodejs command prompt.png)
+
+2. Navigate to the JWST directory in the D: drive
+
+```bash
+cd /D 
+cd path/to/JWST/folder
+```
+
+3. Drag the CSV file you want to reformat into the JSWT folder 
+
+4. Make sure the filename in the last line of code in `index.js` matches the name of the CSV file you want to reformat.
+
+```javascript
+fs.createReadStream(__dirname+'/THIS MUST MATCH INPUT FILENAME.csv').pipe(parser);
+```
+
+5. By default, the output file will be named 'JWST_formatted.txt'. If you want to name it something different, you can change the name of the file by modifying the filename in the `reformattedFilename` variable.
+
+```javascript
+const reformattedFilename = 'CHANGE THIS TO CHANGE THE OUTPUT FILENAME.txt'
+```
+
+6. In the Command Prompt, run the following command:
+
+```bash
+node index.js
+```
+
+7. You should see a lot of "Wrote record" lines in the Command Prompt, and a new file will be created with the name you gave it.
--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-const fs = require('fs'); 
-const { parse } = require('csv-parse');
-const parser = parse();
-const moment = require('moment');
-const util = require('util');
+// Packages and libraries used to do stuff
+const fs = require('fs'); // gives us access to the filesystem
+const { parse } = require('csv-parse'); // a CSV parsing library
+const moment = require('moment'); // a library for working with dates/times
+const util = require('util'); // Node utilities
 
-const errorRows = [];
 
-parser.on('readable', () => {
-    let record;
-    while ((record = parser.read()) !== null) {
-        let JWSTRecord = {};
-        const rawDate = record[0].trim().replace(' 00:00', '');
-        const JWSTDate = moment(rawDate).format('MMM DD')
+const parser = parse(); // creates the CSV parser object
+const reformattedFilename = 'JWST_formatted.txt'; // the default output file name
+
+parser.on('readable', () => { // when the parser can read the input
+    if (fs.existsSync(reformattedFilename)) {
+        // delete any existing output files (that were created last time you ran the script)
+        fs.unlinkSync(reformattedFilename)
+    }
+    let record; // initialize a variable for a line of our input CSV file data
+    while ((record = parser.read()) !== null) { // while the next line of the input CSV file is not null
+        let JWSTRecord = {}; // create the object that will hold our reformatted data
+        const rawDate = record[0].trim().replace(' 00:00', ''); // get the date and chop off the 00:00 bit 
+        const JWSTDate = moment(rawDate).format('MMM DD') // reformat the date to the format we need (i.e. "Aug 09")
         
-        const ra = record[3].trim();
-        const dec = record[4].trim();
+        const ra = record[3].trim(); // get the ra value
+        const dec = record[4].trim(); // get the declination value
 
+        // add all three values to the object. The resulting object will look like this: {label: "Aug 09", ra: "20 04 09.98", dec: "-27 50 18.2"}
         JWSTRecord['label'] = JWSTDate;
         JWSTRecord['ra'] = ra;
         JWSTRecord['dec'] = dec;
+
         try {
-            fs.appendFileSync('JWST_formatted.txt', util.inspect(JWSTRecord) + ",\n")
+            // push the new line into the output file. If the file doesn't exist, fs.appendFileSync will create it
+            fs.appendFileSync(reformattedFilename, util.inspect(JWSTRecord) + ",\n")
             console.log('Wrote record: ', JWSTRecord)
         } catch (err) {
                 console.error("Error writing record: ", record);
-                errorRows.push(record);
             }
     }
 })
 
+// read the input file and pipe it through the parser (this kicks everything off)
 fs.createReadStream(__dirname+'/JWST_Ephemeris.csv').pipe(parser);
